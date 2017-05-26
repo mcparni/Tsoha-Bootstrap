f7b4398f79e55a9a851b447912eae5061da291c5
Miika Pärni <miika.parni@helsinki.fi>
2017-05-26 10:07:28 +0300

    conf change

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ source config/environment.sh
 echo "Luodaan projektikansio..."
 
 # Luodaan projektin kansio
-ssh $USERNAME@users.cs.helsinki.fi "
+ssh $USERNAME@users2017.cs.helsinki.fi "
 cd htdocs
 touch favicon.ico
 mkdir $PROJECT_FOLDER
@@ -21,18 +21,18 @@ echo "Valmis!"
 echo "Siirretään tiedostot users-palvelimelle..."
 
 # Siirretään tiedostot palvelimelle
-scp -r app config lib vendor sql assets index.php composer.json $USERNAME@users.cs.helsinki.fi:htdocs/$PROJECT_FOLDER
+scp -r app config lib vendor sql assets index.php composer.json $USERNAME@users2017.cs.helsinki.fi:htdocs/$PROJECT_FOLDER
 
 echo "Valmis!"
 
 echo "Asetetaan käyttöoikeudet ja asennetaan Composer..."
 
 # Asetetaan oikeudet ja asennetaan Composer
-ssh $USERNAME@users.cs.helsinki.fi "
+ssh $USERNAME@users2017.cs.helsinki.fi "
 chmod -R a+rX htdocs
 cd htdocs/$PROJECT_FOLDER
 wget https://getcomposer.org/download/1.2.4/composer.phar
 php composer.phar install
 exit"
 
-echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa $USERNAME.users.cs.helsinki.fi/$PROJECT_FOLDER"
+echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa $USERNAME.users2017.cs.helsinki.fi/$PROJECT_FOLDER"
