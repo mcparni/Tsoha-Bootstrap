2f465fe43e69f7ba3eecd5c83ffc2c10ca0075cc
Miika Pärni <miika.parni@helsinki.fi>
2017-05-26 12:51:14 +0300

    tietokantataulujen alustus ja testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Admin (name, password) VALUES ('admintest', 'admin_23');
+INSERT INTO Player (name, description, createdon) VALUES ('Pelaaja1', 'Kova luu', NOW());
+INSERT INTO Sport (name, description, sort_order, createdon) VALUES ('Mökkitikka', 'Tikanheitto viidellä tikalla. Suurin tulos voittaa', 1, NOW());
+INSERT INTO Results (player_id, sport_id, result) VALUES (1,1,'38');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,27 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Admin (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Player (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  description varchar(500),
+  createdon DATE
+);
+
+CREATE TABLE Sport (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  description varchar(500),
+  sort_order INTEGER, -- 1 tai 0 määrää pitääkö tuloksia katsoa nousevasti vai laskevasti (1=DESC, 0=ASC)
+  createdon DATE
+);
+
+CREATE TABLE Results (
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES Player(id),
+  sport_id INTEGER REFERENCES Sport(id),
+  result varchar(100)
+);
