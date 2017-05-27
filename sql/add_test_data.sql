ef8c5e0f5fc7ba335ba654c0f12eb7f1556495ae
mcparni <miika.parni@helsinki.fi>
2017-05-27 11:44:08 +0300

    Update add_test_data.sql

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO Admin (name, password) VALUES ('admintest', 'admin_23');
 INSERT INTO Player (name, description, createdon) VALUES ('Pelaaja1', 'Kova luu', NOW());
 INSERT INTO Sport (name, description, sort_order, createdon) VALUES ('Mökkitikka', 'Tikanheitto viidellä tikalla. Suurin tulos voittaa', 1, NOW());
-INSERT INTO Results (player_id, sport_id, result) VALUES (1,1,'38');
+INSERT INTO Results (player_id, sport_id, result, createdon) VALUES (1,1,'38', NOW());
