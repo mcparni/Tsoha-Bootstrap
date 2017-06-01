39203059e79f25e7a0ecf6e5459a538b96ba96a1
Miika Pärni <miika.parni@helsinki.fi>
2017-06-01 11:05:49 +0300

    Sport mallin alustus ja composer riippuvuuksien päivitys

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,38 +1,40 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController{
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
-    }
+	public static function index(){
 
-    public static function lajit(){
-      View::make('lajit.html');
-    }
+		View::make('home.html');
+	}
 
-    public static function laji(){
-      View::make('laji.html');
-    }
+	public static function lajit(){
+		View::make('lajit.html');
+	}
 
-    public static function urheilijat(){
-      View::make('urheilijat.html');
-    }
+	public static function laji(){
+		View::make('laji.html');
+	}
 
-    public static function urheilija(){
-      View::make('urheilija.html');
-    }
+	public static function urheilijat(){
+		View::make('urheilijat.html');
+	}
 
-    public static function tulokset(){
-      View::make('tulokset.html');
-    }
+	public static function urheilija(){
+		View::make('urheilija.html');
+	}
 
-    public static function kirjaudu(){
-      View::make('kirjaudu.html');
-    }
+	public static function tulokset(){
+		View::make('tulokset.html');
+	}
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
-    }
-  }
+	public static function kirjaudu(){
+		View::make('kirjaudu.html');
+	}
+
+	public static function sandbox(){
+    	$tikka = Sport::find(1);
+    	$sports = Sport::all();
+    	Kint::dump($sports);
+    	Kint::dump($tikka);
+	}
+}
---
+++ app/models/Sport.php
@@ -0,0 +1,49 @@
+<?php
+
+class Sport extends BaseModel{
+	public $id, $name, $description, $sort_order, $createdon;
+	
+	public function __construct($attributes){
+		parent::__construct($attributes);
+	}
+
+	public static function all(){
+	    $query = DB::connection()->prepare('SELECT * FROM Sport');
+	    $query->execute();
+	    $rows = $query->fetchAll();
+	    $games = array();
+
+	    foreach($rows as $row){
+			$sports[] = new Sport(array(
+				'id' => $row['id'],
+				'name' => $row['name'],
+				'description' => $row['description'],
+				'sort_order' => $row['sort_order'],
+				'createdon' => $row['createdon']
+			));
+	    }
+
+    	return $sports;
+  	}
+	public static function find($id){
+		$query = DB::connection()->prepare('SELECT * FROM Sport WHERE id = :id LIMIT 1');
+		$query->execute(array('id' => $id));
+		$row = $query->fetch();
+
+		if($row){
+			$sport = new Sport(array(
+				'id' => $row['id'],
+				'name' => $row['name'],
+				'description' => $row['description'],
+				'sort_order' => $row['sort_order'],
+				'createdon' => $row['createdon']
+			));
+			return $sport;
+		}
+
+		return null;
+
+	}
+	
+
+}
